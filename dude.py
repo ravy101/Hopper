6925f1f3ade87d8abb2865e31b6b0916690fdb11
David Warren <ravy101@gmail.com>
2019-04-24 18:26:00 +1000

    no message

--- dude.py
+++ dude.py
@@ -1,4 +1,5 @@
 import pygame
+import hopper
 
 class Dude:
     def __init__(self):
@@ -10,9 +11,33 @@ class Dude:
         self.xacc = 0
         self.yacc = 0
         self.image = pygame.image.load("dude.png")
+        self.gravity = .25
+        self.grounded = True
 
     def update(self):
         self.xpos = self.xpos + self.xvel
+        ##TODO move this to a player game object extension
+        if self.xpos < 0:
+            self.xpos = abs(self.xpos)
+            self.xvel = abs(self.xvel)
+        elif self.xpos > hopper.SCREEN_DIM[0]:
+            self.xpos =  hopper.SCREEN_DIM[0] - (self.xpos -  hopper.SCREEN_DIM[0])
+            self.xvel = -abs(self.xvel)
         self.ypos = self.ypos + self.yvel
         self.xvel = self.xvel + self.xacc
-        self.yvel = self.yvel + self.yacc
+        self.yvel = self.yvel + self.yacc + self.gravity
+
+    def jump(self):
+        self.yvel = self.yvel - 25
+    
+    def move_left(self):
+        if self.grounded:
+            self.xvel = self.xvel - 6
+        else:
+            self.xacc = self.xvel - 2
+    
+    def move_right(self):
+        if self.grounded:
+            self.xvel = self.xvel + 5
+        else:
+            self.xacc = self.xvel + 2
--- hopper.py
+++ hopper.py
@@ -1,16 +1,29 @@
 import pygame
 import dude
+
+#GAME CONSTANTS
+SCREEN_DIM = (600,800)
+BLACK=(0,0,0)
+FPS = 60
+
+def clear_game_object(screen, game_object):
+    pygame.draw.rect(screen, BLACK, game_object.image.get_rect().move(game_object.xpos, game_object.ypos))
+
+def blit_game_object(screen, game_object):
+    screen.blit(game_object.image, (game_object.xpos, game_object.ypos))   
+
 # define a main function
 def main():   
     # initialize the pygame module
     pygame.init()
+    clock = pygame.time.Clock()
     # load and set the logo
     #logo = pygame.image.load("logo32x32.png")
     #pygame.display.set_icon(logo)
     pygame.display.set_caption("minimal program")
-    BLACK=(0,0,0)
+
     # create a surface on screen
-    screen = pygame.display.set_mode((600,800))
+    screen = pygame.display.set_mode(SCREEN_DIM)
     mydude = dude.Dude() 
     # define a variable to control the main loop
     running = True
@@ -19,18 +32,25 @@ def main():
     # main loop
     while running:
         # event handling, gets all event from the event queue
-        
-        pygame.draw.rect(screen, BLACK, mydude.image.get_rect(left=mydude.xpos, top=mydude.ypos))
+        clock.tick(FPS)
+        clear_game_object(screen, mydude)
         mydude.update()
-        screen.blit(mydude.image, (mydude.xpos,mydude.ypos))
+        blit_game_object(screen, mydude)
         pygame.display.flip()
         for event in pygame.event.get():
             # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    mydude.jump()
+                elif event.key == pygame.K_LEFT:
+                    mydude.move_left()
+                elif event.key == pygame.K_RIGHT:
+                    mydude.move_right()
      
-     
+ 
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
 if __name__=="__main__":
